7aa0ecd32497fb4b55b253d0bce9d2f679c189c8
Milan Isac <milan.isac.7@gmail.com>
2025-08-01 23:24:55 +0530

    images added

--- frontend/script.js
+++ frontend/script.js
@@ -8,6 +8,8 @@ async function getRecommendations() {
     try {
         const response = await fetch(`http://localhost:8001/recommend/${encodeURIComponent(movie_name)}`);
         const data = await response.json();
+        // const pic = fetchData(movie_name);
+        // const data1 = await pic.json();
         displayResults(data.recommendations);
     } catch (error) {
         console.log("Error fetching the recommendations: ", error);
@@ -15,7 +17,26 @@ async function getRecommendations() {
     }
 }
 
-function displayResults(movies) {
+async function fetchImage(title) {
+    const url = `https://www.omdbapi.com/?apikey=c192eea2&t=${encodeURIComponent(title)}`;
+
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+
+        if (data.Poster && data.Poster !== "N/A") {
+            return data.Poster;
+        } else {
+            return "https://via.placeholder.com/200x300?text=No+Image";
+        }
+    } catch (error) {
+        console.error(`Error fetching image for ${title}:`, error);
+        return "https://via.placeholder.com/200x300?text=No+Image";;
+    }
+}
+
+
+async function displayResults(movies) {
     const resultsDiv = document.getElementById("results");
     resultsDiv.innerHTML = ""; // Clear previous results
 
@@ -24,7 +45,10 @@ function displayResults(movies) {
         return;
     }
 
-    movies.forEach(movie => {
+    for (const movie of movies) {
+
+        const imgURL = await fetchImage(movie.title);
+
         const movieElement = document.createElement("div");
         movieElement.classList.add("movie-card");
 
@@ -32,14 +56,11 @@ function displayResults(movies) {
             ? movie.genres.map(genre => genre.name).join(", ")
             : "Unknown";
 
-        // console.log(typeof movie.genres);
-        // console.log("Parsed Genres:", genreNames);
-
-        const response = await fetch(`http://localhost:8001/recommend/${encodeURIComponent(movie_name)}`);
-        const movie_title = 
-
         movieElement.innerHTML = `
             <h3>${movie.title}</h3>
+            <div style="display: flex; justify-content: center;">
+            <img src="${imgURL}" alt="${movie.title}" style="width:200px;" />
+            </div>
             <p><strong>Genres:</strong> ${genreNames}</p>
             <p><strong>Tagline:</strong> ${movie.tagline}</p>
             <p><strong>Similarity Score:</strong> ${movie.similarity_score.toFixed(2)}</p>
@@ -47,7 +68,7 @@ function displayResults(movies) {
             <p><strong>Overview:</strong> ${movie.overview}</p>`;
 
         resultsDiv.appendChild(movieElement);
-    });
+    };
 }
 
 document.getElementById("movie_name").addEventListener("keydown", function (event) {
